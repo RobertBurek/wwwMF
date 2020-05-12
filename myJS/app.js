3b188d90866b503953e68f15ce6806b14373aba6
Robert Burek <robertburek@wp.pl>
2020-05-12 13:55:44 +0200

    Refaktor kodu

--- myJS/app.js
+++ myJS/app.js
@@ -1,7 +1,5 @@
 var map = L.map('mapid').setView([52.3415, 20.9764], 10);
 
-console.log("jestem przed");
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
@@ -11,5 +9,3 @@ L.marker([52.3415, 20.9764]).addTo(map)
     .openPopup();
 
 nPopup();
-
-console.log("jestem po");
